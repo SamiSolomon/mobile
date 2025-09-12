7e23db10e23123417f4244dda3bb6e6d39326517
Samuel Solomon <samuelsolomon452@gmail.com>
2025-09-12 11:29:54 +0300

    layout update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,14 @@ import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import SafeScreen from "../components/safeScreen";
 import React, { useEffect } from 'react';
 import { initializeDatabase } from "./db/clients";
+import { initDB as initProductDB } from "./db/product";
+import { initDB as initSalesDB } from "./db/sales";
 
 export default function RootLayout() {
+
+initProductDB();
+initSalesDB();
+
   useEffect(() => {
     initializeDatabase()
       .then(() => console.log('Database initialized successfully'))
